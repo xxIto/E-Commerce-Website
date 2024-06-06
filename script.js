90ea74f317fbf75c70e92a4d018a252c74c57163
Calisto <christofernero@gmail.com>
2024-06-06 22:56:56 +0900

    First Commit

---
+++ script.js
@@ -0,0 +1,53 @@
+// =============== Copy Menu for Mobile ===============
+function copyMenu() {
+    // Copy inside .dpt-cat to .departments
+    var dptCategory = document.querySelector('.dpt-cat');
+    var dptPlace = document.querySelector('.departments');
+    dptPlace.innerHTML = dptCategory.innerHTML;
+
+    // Copy inside nav to nav
+    var mainNav = document.querySelector('.header-nav nav');
+    var navPlace = document.querySelector('.off-canvas nav');
+    navPlace.innerHTML = mainNav.innerHTML;
+
+    // Copy .header-top .wrapper to .thetop-nav
+    var topNav = document.querySelector('.header-top .wrapper');
+    var topPlace = document.querySelector('.off-canvas .thetop-nav');
+    topPlace.innerHTML = topNav.innerHTML;
+}
+copyMenu();
+
+// =============== Show Mobile Menu ===============
+
+
+// =============== Show Sub Menu on Mobile ===============
+const submenu = document.querySelectorAll('.has-child .icon-small');
+submenu.forEach((menu) => menu.addEventListener('click', toggle));
+
+function toggle(e) {
+    e.preventDefault();
+    submenu.forEach((item) => item != this ? item.closest('.has-child').classList.remove('expand') : null);
+    if (this.closest('.has-child').classList != 'expand');
+    this.closest('.has-child').classList.toggle('expand');
+}
+
+
+/* ===============
+ *
+ *  KODE PENGGANTI
+ *
+ ============== */
+
+// =============== Show Sub Menu on Mobile ===============
+// const submenu = document.querySelectorAll('.has-child .icon-small');
+// submenu.forEach((menu) => menu.addEventListener('click', toggle));
+
+// function toggle(e) {
+//     e.preventDefault();
+//     submenu.forEach((item) => {
+//         if (item !== this) {
+//             item.closest('.has-child').classList.remove('expand');
+//         }
+//     });
+//     this.closest('.has-child').classList.toggle('expand');
+// }
